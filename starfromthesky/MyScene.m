a4bfefc6d7a9cf1a5341f2ddeb7260157294fbd0
xehivs <pawel@ksienie.com>
2013-09-13 18:47:08 +0200

    Piesek!

--- starfromthesky/MyScene.m
+++ starfromthesky/MyScene.m
@@ -9,54 +9,54 @@
 #import "MyScene.h"
 
 @implementation MyScene {
-    SKSpriteNode *sprite;
+    SKSpriteNode *dog;
+    SKAction *walkingDog;
+    SKAction *standingDog;
+}
+
+- (void) makeDog {
+    SKTexture *dogTexture = [SKTexture textureWithImageNamed:@"dog"];
+    SKTexture *dogWalk1Texture = [SKTexture textureWithImageNamed:@"dog_walk1"];
+    SKTexture *dogWalk2Texture = [SKTexture textureWithImageNamed:@"dog_walk2"];
+    
+    walkingDog = [SKAction animateWithTextures:@[dogTexture,dogWalk1Texture,dogWalk2Texture]
+                                  timePerFrame:.1 resize:NO restore:YES];
+    
+    dog = [SKSpriteNode spriteNodeWithTexture:dogTexture];
 }
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
-        
-        self.backgroundColor = [SKColor redColor];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        self.backgroundColor = [SKColor whiteColor];
 
-        sprite.position = CGPointMake(CGRectGetMidX(self.frame),
-                                      -100);
+        [self makeDog];
 
-        myLabel.text = @"Star from the Sky!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [sprite runAction:[SKAction moveToY:100 duration:1]];
-        
-        [self addChild:myLabel];
-        [self addChild:sprite];
-        
-     }
+        dog.position = CGPointMake(0,144);
+
+        NSLog(@"%@", dog);
+        [self addChild:dog];
+    }
     return self;
 }
 
-- (void) steerToX:(CGFloat) x{
-    [sprite runAction:[SKAction moveToX:x
-                               duration:.25]];
+- (void) moveDogToLocation:(CGPoint) location{
+    [dog removeAllActions];
+    
+    if (location.x > dog.position.x) [dog setXScale:-1];
+    else [dog setXScale:1];
+    
+    [dog runAction:[SKAction repeatActionForever:walkingDog]];
+    [dog runAction:[SKAction moveTo:location duration:2]
+        completion:^(){
+            NSLog(@"%@", dog);
+            [dog removeAllActions];
+        }];
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-    /* Called when a touch begins */
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        [self steerToX:location.x];
-    }
-}
-
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-    /* Called when a touch begins */
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        [self steerToX:location.x];
-    }
+    CGPoint location = [[touches anyObject] locationInNode:self];
+    [self moveDogToLocation:CGPointMake(location.x, 144)];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
